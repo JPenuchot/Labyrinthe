a747aff1a677da1f182f86396a26314c25b301d5
DavidBrd <david-biard@hotmail.fr>
2018-05-06 17:07:38 +0200

    refresh walk map

--- src/Labyrinthe/refreshWalkMap.cpp
+++ src/Labyrinthe/refreshWalkMap.cpp
@@ -1,11 +1,26 @@
+#include <algorithm>
+
 #include "../Labyrinthe.h"
+
+using namespace std;
 
 void Labyrinthe::refreshWalkMap()
 {
   //  Murs
+	for_each(this->_walls, this->_walls + this->_nwall, [&](auto w) {
+		for(int i = w._y1; i < w._y2; i++) {
+			for(int j = w._x1; j < w._x2; j++) {
+				this->walkable(i, j) = false;
+			}
+		}
+	});
 
   //  Boxes
+	for_each(this->_boxes, this->_boxes + this->_nboxes, [&](auto b) {
+		this->walkable(b._y, b._x) = false;
+	});
 
   //  Trésor
+	this->walkable(this->_treasor._y, this->_treasor._x) = false;
 
 }
